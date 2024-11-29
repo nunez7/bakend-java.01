6b63c15edddd98acc4e384257c543904c82d1af3
Felix Nunez <felix.nunez@vass.com.mx>
2024-11-29 15:14:10 -0700

    Plantilla sin code

---
+++ src/main/java/mx/com/vass/BackendJava01Application.java
@@ -0,0 +1,13 @@
+package mx.com.vass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJava01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJava01Application.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/vass/BackendJava01ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.vass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJava01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
